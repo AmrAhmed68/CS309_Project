294e4c4d59a64f94a9f68cac8d991af526e48781
AboBakr <amrahmed682003@gmail.com>
2023-12-08 14:23:43 +0200

    Amr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Search/>
       {/* <Slider/> */}
       <Routes>
             <Route path="/" element={<HomeScreen />} />
