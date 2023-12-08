48cea816d738b2ebc20343719f1a3e479fce6d63
AboBakr <amrahmed682003@gmail.com>
2023-12-08 15:11:34 +0200

    amr

--- frontend/src/Components/product.js
+++ frontend/src/Components/product.js
@@ -1,33 +1,49 @@
-// const Products = require('./Schema')
-// const Laps_Prooduct = require('./Laps_Product');
+// import Input from "./Input";
+// import Laps_Product from "./Laps_Product";
 
-// const DefaultData = async () => {
-//     try {
+// function Product() {
+//     const products = Laps_Product.map((product) => {
+//         return(
+//             <Input key={product.id} product={product}/>
+//         )
+//     })
+//     return <div> 
+//         {products}
+//          </div>
+//   }
+  
+//   export default Product;
 
-//         await Products.deleteMany({});
-
-//         const storeData = await Products.insertMany(Laps_Prooduct);
-//         console.log(storeData);
-//     } catch (error) {
-//         console.log("error" + error.message);
-//     }
-// };
-
-// module.exports = DefaultData;
 // import { useState } from "react";
-import Input from "./Input";
-import Laps_Product from "./Laps_Product";
+import React from "react";
+import productData from "./Laps_Product";
+import "./Input.css";
 
 function Product() {
-    const products = Laps_Product.map((product) => {
-        return(
-            <Input key={product.id} product={product}/>
-        )
-    })
-    return <div> 
-        {products}
-         </div>
+    const products = productData();
+  
+    return (
+      <div>
+        <ul>
+          {products.map((product) => (
+            <section className="card-container">
+                <section className="card">
+                    <img src = {product.images} alt="" className='card-img'/>
+                        <div className='details'>
+                            <h3> {product.brand} </h3>
+                <h2 className='card-title'>{product.title}</h2>
+                <p className='description-card'> {product.description} </p>
+                <p className='card-price'> price : {product.price} EGP </p>
+                <button  className="bnt" onClick={() => { 
+                    console.log('you are going to buy ', product.title , product.price)
+                }}> buy </button>
+            </div>
+        </section>
+    </section>
+          ))}
+        </ul>
+      </div>
+    );
   }
   
   export default Product;
-  
